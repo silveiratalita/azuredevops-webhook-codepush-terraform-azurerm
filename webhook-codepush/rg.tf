c04663d05a327ea4928e85ff153a61603427562c
Julio Paiva <julio.paiva@kroton.com.br>
2021-03-26 16:01:08 -0300

    Alteração RG

--- webhook-codepush/rg.tf
+++ webhook-codepush/rg.tf
@@ -1,12 +1,12 @@
-# resource "azurerm_resource_group" "rg_webhookpush" {
-#   name     = "${var.prefix_name_webhookpush}-${var.environment}"
-#   location = var.location[var.environment]
+data "azurerm_resource_group" "rg_webhookpush" {
+  name     = "${var.prefix_name_webhookpush}-${var.environment}"
+  location = var.location[var.environment]
 
-#   tags = local.tags
-#   lifecycle {
-#     ignore_changes = [
-#       tags,
-#     ]
-#     prevent_destroy = true
-#   }
-# }
+#  tags = local.tags
+#  lifecycle {
+#    ignore_changes = [
+#      tags,
+#    ]
+#   prevent_destroy = true
+#  }
+}
