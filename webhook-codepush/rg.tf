655535ec870fa32e85653d80752e36e96d91057b
Talita <talita.silveira@kroton.com.br>
2021-03-26 18:56:34 -0300

    teste

--- webhook-codepush/rg.tf
+++ webhook-codepush/rg.tf
@@ -1,6 +1,6 @@
 data "azurerm_resource_group" "system-team" {
   name     = var.resource_group_webhookpush
-  # location = var.location
+  location = var.location[var.environment]
   #  tags = local.tags
   #  lifecycle {
   #    ignore_changes = [
